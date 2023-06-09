2ee2f9801a3678cf4f7ec592bae2ec9a6cfb143b
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2023-06-08 23:35:13 -0300

    modified:   app.js 	modified:   package.json 	modified:   routes/db/products.json 	modified:   routes/get.js 	modified:   routes/post.js

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,29 @@
 const express =require('express')
+const fs = require('fs')
+const routes = require('./get')
+
 const router = express.Router()
 
-const data = require('./db/products')
+const readFile = () => {
+    const data = fs.readFileSync('./routes/db/products.json')
+    return JSON.parse(data)
+}
 
-router.post
+const writeFile = (data) => {
+    const updateFile = JSON.stringify(data)
+    fs.writeFileSync('./routes/db/products.json', updateFile, 'utf-8')
 
-module.exports = router
+}
+
+routes.post('/add', (req, res) =>{
+    const { id, name, color, price } = req.body
+    const currentData = readFile()
+    currentData.push({ id, name, color, price })
+    writeFile(currentData)
+    res.send({ id, name, color, price })      
+})
+
+
+module.exports = routes
+
+
